a15eefe228b5bbef87e3920d5ba5156bbc95f935
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-02-22 18:32:18 -0300

    Adiciona exercicio 4

---
+++ Ex4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] nums = new int[5];
+            int menor = int.MaxValue;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Digite o {i + 1}º número");
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (nums[i] < menor)
+                {
+                    menor = nums[i];
+                }
+            }
+
+            Console.WriteLine($"O menor número digitado foi {menor}");
+        }
+    }
+}
