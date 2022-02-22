89841ad24f2d36d6b8eab4c614c844ac02dad721
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-02-22 18:08:49 -0300

    Adiciona exercício 3

---
+++ Ex3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] nums = new int[5];
+            int maior = 0;
+
+            for (int i = 0; i < 5; i++)
+            {   
+                Console.WriteLine($"Digite o {i+1}º número");
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (nums[i] > maior)
+                {
+                    maior = nums[i];
+                }
+            }
+
+            Console.WriteLine($"O maior número é digitado foi {maior}");
+        }
+    }
+}
