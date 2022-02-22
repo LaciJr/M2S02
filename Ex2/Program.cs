a166ff42baec7cefb68d3c6c612ad8c5b70fb51d
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-02-22 12:43:03 -0300

    Adiciona Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Digite a temperatura em graus celsius:");
+            var c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"A temperatura em graus Fahrenheit é {c * 1.8 + 32}");
+        }
+    }
+}
