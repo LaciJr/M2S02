aa5428de911f627b5f173d351e503b419c4941cd
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-02-21 22:30:13 -0300

    Add project files.

---
+++ Ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var r = Console.ReadLine();
+            int ra = Convert.ToInt32(r);
+            Console.WriteLine(ra*ra*Math.PI);
+        }
+    }
+}
