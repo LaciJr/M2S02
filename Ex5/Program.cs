1640401f531c5147ac2b82e368dc69b12b9d61dc
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-02-22 18:59:49 -0300

    Adiciona exercicio 5

---
+++ Ex5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> nums = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Digite o {i + 1}º número");
+                nums.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            List<int> numsOrd = nums.OrderByDescending(e => e).ToList();
+
+            Console.WriteLine("Números digitados ordenados:");
+            foreach (var num  in numsOrd)
+            {
+                Console.Write(num);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
