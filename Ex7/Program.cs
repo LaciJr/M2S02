480b42d4b6d2b3f07fe2ad209845577fd662a45c
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-02-22 22:17:39 -0300

    Adiciona exercicio 7

---
+++ Ex7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex7
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Informe o número a ser invertido:");
+            var num = Console.ReadLine();
+            var invertido = "";
+
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                invertido += num[i];
+            }
+
+            Console.WriteLine($"Valor invertido: {invertido}");
+        }
+    }
+}
