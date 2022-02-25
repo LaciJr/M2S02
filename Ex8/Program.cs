13615a3d3f0154eeefa35b806f01023f7bd9ff9c
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-02-24 22:27:11 -0300

    Atualiza exercicio 8

--- Ex8/Program.cs
+++ Ex8/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace Ex8
 {
@@ -10,33 +8,22 @@ namespace Ex8
     {
         static void Main()
         {
-            List<Alunos> listaNotas = new List<Alunos>();
-
-
             string path = @"C:\Users\laci-\Documents\GitHub\M2S02\Ex8\notas.json";
+            string json = File.ReadAllText(path);
+            dynamic listaNotas = JsonConvert.DeserializeObject(json);
+            int alunos = 0;
 
-            using (StreamReader sr = File.OpenText(path))
+            Console.WriteLine("Alunos com nota menor que 5:");
+            foreach (var item in listaNotas)
             {
-                var s = sr.ReadToEnd();
-                listaNotas = JsonSerializer.Deserialize<List<Alunos>>(s);
+                if (item.Nota < 5)
+                {
+                    Console.WriteLine(item.Nome);
+                    alunos++;
+                }
             }
 
-            if (listaNotas.Any(e => e.Nota < 5))
-            {
-                var AlunosReprovados = listaNotas.FindAll(e => e.Nota < 5);
-                Console.WriteLine("Alunos com nota menor que 5:");
-                AlunosReprovados.ForEach(a => Console.WriteLine(a.Nome));
-            } 
-            else
-            {
-                Console.WriteLine("Nenhum aluno com nota menor que 5!");
-            }
-        }
-
-        public class Alunos
-        {
-            public string Nome { get; set; }
-            public int Nota { get; set; }
+            if(alunos == 0) Console.WriteLine("Nenhum aluno com nota menor que 5!");
         }
     }
 }
